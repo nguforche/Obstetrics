3907b6e8cd26001fe699591fa8227e856dfe1b31
nguforche <nguforche@gmail.com>
2019-03-06 05:21:45 -0600

    updates

--- inst/www/ObstetricsApp.js
+++ inst/www/ObstetricsApp.js
@@ -3,15 +3,15 @@ ocpu.seturl("//public.opencpu.org/ocpu/library/stats/R")
 
 //some example data
 //to run with different data, edit and press Run at the top of the page
-var mydata = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+var mydata = [1,2,3,4,5,5,999,5,4,4,3,2,1];
 
-//call R function: stats::sd(x=data)
+//call R function: stats::var(x=data)
 $("#submitbutton").click(function(){
     
-    var req = ocpu.rpc("sd",{
+    var req = ocpu.rpc("lowess",{
         x : mydata
     }, function(output){
-        alert("Standard Deviation equals: " + output);
+        $("code").text(output.y.join("\n"));
     }); 
 
     //optional
